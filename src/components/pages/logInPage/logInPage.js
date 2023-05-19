b88db90661fe55fac8867c08ab444635a6b4839c
Peter_Solti <ptsolti@gmail.com>
2023-05-19 14:06:25 +0200

    add mui

--- src/components/pages/logInPage/logInPage.js
+++ src/components/pages/logInPage/logInPage.js
@@ -1,6 +1,32 @@
+import Grid from "@mui/material/Grid";
+
 function LogInPage() {
   return (
     <div className="LogInPage">
+      <Grid container>
+        <Grid item xl={8}>
+          <p>
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma
+          </p>
+        </Grid>
+        <Grid item xl={4}>
+          <p>
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma alma alma alma alma alma alma alma alma alma alma alma
+            alma alma
+          </p>
+        </Grid>
+      </Grid>
       <p>Login</p>
     </div>
   );
